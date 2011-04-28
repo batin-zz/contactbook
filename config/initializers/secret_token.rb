cede5d7e7c35261582374d8fb79f316991d7ef34
batin <batin.oleg@gmail.com>
2011-04-28 10:31:27 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contactbook::Application.config.secret_token = '0607cea244506aa788d896b47f23e7e5050cf9e1d8b195f9cb986322b8bdb82dfce09dc9aef41806a5c327e6b1e37afd497917b774deb1d3c32d69a0cfce3d67'
