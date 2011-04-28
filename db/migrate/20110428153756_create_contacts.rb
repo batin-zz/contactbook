5ef412105b65c3162cf9110b1cea307903706293
batin <batin.oleg@gmail.com>
2011-04-28 10:38:41 -0500

    Contact scaffold

---
+++ db/migrate/20110428153756_create_contacts.rb
@@ -0,0 +1,17 @@
+class CreateContacts < ActiveRecord::Migration
+  def self.up
+    create_table :contacts do |t|
+      t.string :name
+      t.string :email
+      t.string :web
+      t.text :description
+      t.string :type
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :contacts
+  end
+end
